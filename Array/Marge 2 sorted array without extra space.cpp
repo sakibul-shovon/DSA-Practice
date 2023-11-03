e1a71f0fce4a045357e84ddbc39294b0c39d6215
shovon-coder <114784022+shovon-coder@users.noreply.github.com>
2023-11-04 05:20:52 +0600

    commit

---
+++ Array/Marge 2 sorted array without extra space.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long a[], long long b[], int n, int m) 
+        { 
+            int left = n-1;
+            int right = 0;
+            
+            while(left >=0 && right<m){
+                if(a[left] >= b[right] ){
+                    swap(a[left],b[right]);
+                    left--;
+                    right++;
+                }
+                
+                else{
+                    break;
+                }
+            }
+            sort(a,a+n);
+            sort(b,b+m);
+             
+        } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
